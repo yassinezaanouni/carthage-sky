d13fa4222ffbd8d0f30a36851ff0b410006fd6c2
Yassine Zaaanouni <yassinezaanouni@gmail.com>
2024-01-22 15:09:42 +0100

    fix chosse us page container

--- components/ChooseUs/ChooseUsPage.js
+++ components/ChooseUs/ChooseUsPage.js
@@ -1,10 +1,10 @@
 import Image from "next/image";
 import Link from "next/link";
+import { Parallax } from "react-scroll-parallax";
 import SvgArrowLong from "../../public/svg-components/ArrowLong";
 import Contactus from "../Index/Contactus";
 import CardsSelector from "../Widgets/CardsSelector";
 import RowCard from "../Widgets/RowCard";
-import { Parallax } from "react-scroll-parallax";
 
 const ChooseUsPage = () => {
   const chooseUs = [
@@ -49,21 +49,21 @@ const ChooseUsPage = () => {
       description:
         "Through compliance with legislation and regulations, Carthage Sky aims to bequeath a healthy work environment by identifying risks to occupational health & safety, managing risk and providing sufficient, healthy and secure resources and sub-structure for our employees.",
     },
-    {
-      title: "Sustainability",
-      description:
-        "For us, sustainability means achieving our growth ambitions, in the following way: taking the present and future needs of society into account; offering eco-efficient and innovative services to our customers; acting in an ethical, transparent manner and in accordance with the laws and regulations; providing a motivating, healthy and safe work environment for our direct and indirect employees; supporting our local communities and encouraging their progress.",
-    },
-    {
-      title: "Data and Cyber Security",
-      description:
-        "Carthage Sky declares a commitment to the following issues; \n\n      Ensuring data confidentiality in all our processes\n      Protecting corporate security and trademark image, \n      Protecting all information belonging to the partners, \n      Providing compliance as defined by contract with third parties, \n      Providing the sustainable continuation of the main and support processes without \n      unwarranted lapses. ",
-    },
-    {
-      title: "Social Responsibility and employees",
-      description:
-        "The social aspect is one of the driving factors in our company, this is why, and we have implemented a combined process, adding a social value to our environmental actions.\n\nThrough implementing in our company a waste segregation method to reduce our waste, we are committed to give our waste a valuable purpose, therefore, we give our segregated waste to waste collector NGO, to sell and to help its revenues schooling marginalized children or creating new job opportunities for recycling companies.\n\nCarthage Sky, due to its awareness of social responsibility, aims to participate in the society in which it operates beyond its commercial interests. \n\nThis policy is manifested through its employees who have full awareness of the values being put forward through their work and the regulations to be followed. They adopt the company values as their own, work to solve problems, share knowledge, willingly participate in teamwork, aim to improve and are always ready to take part in every kind of physical, medical or mental challenge",
-    },
+    // {
+    //   title: "Sustainability",
+    //   description:
+    //     "For us, sustainability means achieving our growth ambitions, in the following way: taking the present and future needs of society into account; offering eco-efficient and innovative services to our customers; acting in an ethical, transparent manner and in accordance with the laws and regulations; providing a motivating, healthy and safe work environment for our direct and indirect employees; supporting our local communities and encouraging their progress.",
+    // },
+    // {
+    //   title: "Data and Cyber Security",
+    //   description:
+    //     "Carthage Sky declares a commitment to the following issues; \n\n      Ensuring data confidentiality in all our processes\n      Protecting corporate security and trademark image, \n      Protecting all information belonging to the partners, \n      Providing compliance as defined by contract with third parties, \n      Providing the sustainable continuation of the main and support processes without \n      unwarranted lapses. ",
+    // },
+    // {
+    //   title: "Social Responsibility and employees",
+    //   description:
+    //     "The social aspect is one of the driving factors in our company, this is why, and we have implemented a combined process, adding a social value to our environmental actions.\n\nThrough implementing in our company a waste segregation method to reduce our waste, we are committed to give our waste a valuable purpose, therefore, we give our segregated waste to waste collector NGO, to sell and to help its revenues schooling marginalized children or creating new job opportunities for recycling companies.\n\nCarthage Sky, due to its awareness of social responsibility, aims to participate in the society in which it operates beyond its commercial interests. \n\nThis policy is manifested through its employees who have full awareness of the values being put forward through their work and the regulations to be followed. They adopt the company values as their own, work to solve problems, share knowledge, willingly participate in teamwork, aim to improve and are always ready to take part in every kind of physical, medical or mental challenge",
+    // },
   ];
   return (
     <>
@@ -71,10 +71,13 @@ const ChooseUsPage = () => {
         <div className="container">
           <h1 className="text-3xl font-bold lg:text-5xl">Why Choose Us</h1>
           <p className="mx-auto mt-5 mb-[1.5625rem] max-w-[46.875rem] text-lg">
-            As an aviation business player, we are committed to the highest standards of service ensuring we have all
-            the right processes and systems in place. We not only meet aviation standards, we exceed them.
+            As an aviation business player, we are committed to the highest
+            standards of service ensuring we have all the right processes and
+            systems in place. We not only meet aviation standards, we exceed
+            them.
             <br />
-            Carthage Sky acheived ISO9001, ISO14001 & OHSAS 18001 certifications in February 2019.
+            Carthage Sky acheived ISO9001, ISO14001 & OHSAS 18001 certifications
+            in February 2019.
           </p>
           <Link href="#company-governance">
             <a className="f-ai-c group justify-center gap-3 text-[1.375rem] text-primary-400 hover:font-semibold hover:text-primary-400/90">
@@ -86,19 +89,31 @@ const ChooseUsPage = () => {
         <div className="f-ai-c relative mt-[3.2313rem] h-[25rem] w-full justify-center md:h-[34.5625rem]">
           <Image src="/imgs/services-page/bg.webp" alt="sky" layout="fill" />
           <Parallax translateX={["0px", "-50px"]} speed={5}>
-            <Image src="/imgs/choose-us/plane.webp" alt="sky" width={929.86} height={422.48} />
+            <Image
+              src="/imgs/choose-us/plane.webp"
+              alt="sky"
+              width={929.86}
+              height={422.48}
+            />
           </Parallax>
         </div>
       </section>
 
       {/* Accreditations */}
       <section className="container text-center">
-        <h2 className="mb-12 text-3xl font-bold lg:text-5xl">Our Accreditations</h2>
+        <h2 className="mb-12 text-3xl font-bold lg:text-5xl">
+          Our Accreditations
+        </h2>
         <div className="flex flex-wrap justify-center gap-8 md:justify-between">
           <div className="">
             <h3 className="text-2xl">Quality</h3>
             <div className="border border-black">
-              <Image src="/imgs/choose-us/accreditations/quality.webp" alt="quality" width={321} height={448} />
+              <Image
+                src="/imgs/choose-us/accreditations/quality.webp"
+                alt="quality"
+                width={321}
+                height={448}
+              />
             </div>
           </div>
           <div className="">
@@ -125,22 +140,30 @@ const ChooseUsPage = () => {
           </div>
         </div>
 
-        <RowCard className="mt-[6.125rem] md:mt-[8.125rem] lg:mt-[12.125rem]" image="/imgs/choose-us/card1.webp">
+        <RowCard
+          className="mt-[6.125rem] md:mt-[8.125rem] lg:mt-[12.125rem]"
+          image="/imgs/choose-us/card1.webp"
+        >
           <h3 className="mt-1 mb-3 text-2xl font-bold leading-snug md:mb-6 lg:text-[2.125rem]">
             How We Meet International Standards:
           </h3>
           <p className="text-lg md:mb-6">
-            These accreditations applaud us for meeting international standards and demonstrating that we are able to
-            provide highest level of service quality, Environment , safety and security to customers. They honor our
-            dedication to prevent pollution and preserve environement for future generations, as well as our commitment
+            These accreditations applaud us for meeting international standards
+            and demonstrating that we are able to provide highest level of
+            service quality, Environment , safety and security to customers.
+            They honor our dedication to prevent pollution and preserve
+            environement for future generations, as well as our commitment
             regarding safety and security in all activities.
             <br />
             <br />
-            Our certification was issued on the 13thFebruary 2019 and is valid until 12thFebruary 2022. All audits were
-            performed by TUV Intercert Germany.
+            Our certification was issued on the 13thFebruary 2019 and is valid
+            until 12thFebruary 2022. All audits were performed by TUV Intercert
+            Germany.
           </p>
           <Link href="#">
-            <a className="text-lg text-primary-400 transition-all hover:text-primary-400/90">Code of Conduct</a>
+            <a className="text-lg text-primary-400 transition-all hover:text-primary-400/90">
+              Code of Conduct
+            </a>
           </Link>
         </RowCard>
       </section>
@@ -152,7 +175,8 @@ const ChooseUsPage = () => {
         <div
           className="absolute inset-0"
           style={{
-            background: "linear-gradient(-90deg, rgba(70, 186, 235, 0) 0%, rgba(70, 186, 235, 0.918) 100%)",
+            background:
+              "linear-gradient(-90deg, rgba(70, 186, 235, 0) 0%, rgba(70, 186, 235, 0.918) 100%)",
             zIndex: 1,
           }}
         />
@@ -164,21 +188,33 @@ const ChooseUsPage = () => {
             zIndex: 1,
           }}
         >
-          <h2 className="text-3xl font-bold lg:text-5xl ">Company Governance</h2>
+          <h2 className="text-3xl font-bold lg:text-5xl">Company Governance</h2>
           <p className="mt-6 max-w-[35rem] text-lg">
-            Our company is guided by a set of principles, initiatives and management structures which enable an
-            integrated vision and agile decision making process.
+            Our company is guided by a set of principles, initiatives and
+            management structures which enable an integrated vision and agile
+            decision making process.
           </p>
         </div>
-        <Image src="/imgs/choose-us/cloud2.webp" alt="" name="services-banner" layout="fill" />
+        <Image
+          src="/imgs/choose-us/cloud2.webp"
+          alt=""
+          name="services-banner"
+          layout="fill"
+        />
         <div className="absolute -bottom-2 right-0">
           <Parallax translateX={["0px", "10px"]} speed={-2}>
-            <Image src="/imgs/services/plane.webp" alt="" name="services-banner" width={866} height={568} />
+            <Image
+              src="/imgs/services/plane.webp"
+              alt=""
+              name="services-banner"
+              width={866}
+              height={568}
+            />
           </Parallax>
         </div>
       </div>
 
-      <section id="company-governance">
+      <section id="company-governance" className="container">
         <CardsSelector iconFolder="choose-us" items={chooseUs} />
       </section>
       <Contactus />
